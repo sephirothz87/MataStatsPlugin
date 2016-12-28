9e3dfebc9144aa950da57de5bfaeb65839df5d9c
zhong.zhicong <zhong.zhicong@newphoria.co.jp>
2016-12-28 13:06:25 +0800

    no message

--- MatchupPlugin.js
+++ MatchupPlugin.js
@@ -1,30 +1,15 @@
-//将下记页面的对局胜率表以csv形式输出
+//将以下页面的对局胜率表以csv形式输出
 //http://metastats.net/archetype/matchup/
-
-
-// counter = 0
-// console.log($("table").find($(".highlightrow")))
 rows = $("table").find($(".highlightrow"))
 
-// for(row in $("table").find($(".highlightrow"))){
 result = ""
 for (i = 0; i < rows.length; i++) {
-    // console.log(row.text())
-    // counter++
-    // console.log(counter)
-    // console.log(rows[i])
-
-
-    // console.log($(rows[i]).find($("td")))
     cells = $(rows[i]).find($("td"))
     for (j = 0; j < cells.length; j++) {
         if ($(cells[j]).hasClass("playerarch")) {
-            // console.log("deck name")
-            // console.log($(cells[j]).text())
             result += $(cells[j]).text()
         } else {
             cells_div = $(cells[j]).find($("div"))
-                // console.log(cells_div)
             if ($(cells_div).hasClass("deckwinrate")) {
                 result += "," + $(cells_div).attr("data-original-title").match(/\d+\%/)
             } else {
@@ -36,8 +21,3 @@ for (i = 0; i < rows.length; i++) {
 }
 
 console.log(result)
-
-// str = "Games: 1598<br>Jade Druid: 41%<br>Midrange Hunter: 59%"
-
-// // console.log(str.search(/\d\%/))
-// console.log(str.match(/\d+\%/g))
